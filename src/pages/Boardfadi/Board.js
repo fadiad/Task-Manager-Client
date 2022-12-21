f12008e4730da734caeb0bbf2a96b1797c4aa653
khaledwani27 <wanikhaled27@gmail.com>
2022-12-21 11:51:35 +0200

    working on the add status and add item on board featuers

--- src/pages/Boardfadi/Board.js
+++ src/pages/Boardfadi/Board.js
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react'
 import { observer, inject } from 'mobx-react'
-import Status from '../../components/Board/Status'
+import Status from '../../components/Boardfadi/Status'
 import './board.css'
-import AddStatus from '../../components/Board/AddStatus'
+import AddStatus from '../../components/Boardfadi/AddStatus'
 import { DragDropContext } from "react-beautiful-dnd";
 
 const Board = (props) => {
